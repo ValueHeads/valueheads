a8803c74605c3ecfdf5f0a0601dcb6fa707bdf7b
Amer <amerk86@gmail.com>
2021-10-22 11:44:23 +1000

    added link to share component

--- .vuepress/components/CtaShare.vue
+++ .vuepress/components/CtaShare.vue
@@ -1,30 +1,69 @@
 <template>
-  <ul class="grid grid-cols-4 gap-4 lg:grid-cols-8">
-    <li v-for="site in sites" :key="site.question">
-      <a
-        @click="$emit('done')"
-        :href="site.link"
-        target="_blank"
-        class="flex justify-center items-center w-12 h-12 text-white bg-indigo-500 rounded-md  noopener hover:bg-yellow-600"
-      >
-        <component
-          :is="site.icon"
-          :icon="site.faIcon"
-          class="w-6 h-6"
-          aria-hidden="true"
-        />
-      </a>
-    </li>
-    <li>
-      <base-share-link
-        @click="$emit('done')"
-        class="flex justify-center items-center w-12 h-12 text-white bg-indigo-500 rounded-md  noopener hover:bg-indigo-800"
-        title="Check out ValueHeads.org, Follow-the-Value Entrepreneurship Course. Free!"
-      >
-        <share-icon size="1.5x"></share-icon>
-      </base-share-link>
-    </li>
-  </ul>
+  <div>
+    <ul class="grid grid-cols-4 gap-4 lg:grid-cols-8">
+      <li v-for="site in sites" :key="site.question">
+        <a
+          @click="$emit('done')"
+          :href="site.link"
+          target="_blank"
+          class="
+            flex
+            justify-center
+            items-center
+            w-12
+            h-12
+            text-white
+            bg-indigo-500
+            rounded-md
+            noopener
+            hover:bg-yellow-600
+          "
+        >
+          <component
+            :is="site.icon"
+            :icon="site.faIcon"
+            class="w-6 h-6"
+            aria-hidden="true"
+          />
+        </a>
+      </li>
+      <li>
+        <base-share-link
+          @click="$emit('done')"
+          class="
+            flex
+            justify-center
+            items-center
+            w-12
+            h-12
+            text-white
+            bg-indigo-500
+            rounded-md
+            noopener
+            hover:bg-indigo-800
+          "
+          title="Check out ValueHeads.org, Follow-the-Value Entrepreneurship Course. Free!"
+        >
+          <share-icon size="1.5x"></share-icon>
+        </base-share-link>
+      </li>
+    </ul>
+
+    <button
+      @click.prevent="$emit('done')"
+      class="
+        inline-block
+        mt-5
+        text-base
+        font-medium
+        text-indigo-700
+        hover:underline
+        hover:text-indigo-500
+      "
+    >
+      I posted already! 😎
+    </button>
+  </div>
 </template>
 
 <script>

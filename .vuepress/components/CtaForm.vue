c8406a6b9cdbe961a73bfc629691b709dee8016f
Amer <amerk86@gmail.com>
2021-10-29 23:51:05 +1000

    Using Zoho Campaigns - no netlify function

--- .vuepress/components/CtaForm.vue
+++ .vuepress/components/CtaForm.vue
@@ -1,31 +1,38 @@
 <template>
-  <form
-    @submit.prevent="submitForm()"
-    class="box-content mx-auto mt-5  sm:max-w-xl sm:w-full sm:grid sm:grid-cols-2 sm:gap-4"
-  >
-    <div class="min-w-0">
-      <label for="hero-email" class="sr-only">Email address</label>
-      <input
-        id="hero-email"
-        type="email"
-        v-model="email"
-        class="block px-5 py-3 w-full text-base"
-        placeholder="Enter your email"
-      />
-    </div>
-    <div class="mt-4 sm:mt-0 sm:ml-3">
-      <button
-        :disabled="!isEmailValid || isSubmitting"
-        type="submit"
-        class="block px-5 py-3 w-full text-base font-medium text-white bg-indigo-500 rounded-md border border-transparent shadow  hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
-      >
-        Join ValueHeads
-      </button>
-    </div>
-  </form>
+  <div>
+    <form
+      @submit.prevent="submitForm()"
+      class="box-content mx-auto mt-5  text-olive-800 sm:max-w-xl sm:w-full sm:grid sm:grid-cols-2 sm:gap-4"
+    >
+      <div class="min-w-0">
+        <label for="hero-email" class="sr-only">Email address</label>
+        <input
+          id="hero-email"
+          type="email"
+          v-model="email"
+          class="block px-5 py-3 w-full text-base"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div class="mt-4 sm:mt-0 sm:ml-3">
+        <button
+          :disabled="!isEmailValid || isSubmitting"
+          type="submit"
+          class="block px-5 py-3 w-full text-base font-medium text-white bg-indigo-500 rounded-md border border-transparent shadow  hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+        >
+          Join ValueHeads
+        </button>
+      </div>
+    </form>
+
+    <p class="mt-4 text-sm text-olive-500">
+      You'll receive TWO emails. Email confirmation, then my email.
+    </p>
+  </div>
 </template>
 
 <script>
+const qs = require("qs");
 const axios = require("axios");
 
 // Docs https://github.com/manishsaraan/email-validator
@@ -34,7 +41,7 @@ var validator = require("email-validator");
 export default {
   data() {
     return {
-      email: "",
+      email: "amerk86@gmail.com",
       isSubmitting: false,
     };
   },
@@ -46,18 +53,36 @@ export default {
   methods: {
     submitForm() {
       this.isSubmitting = true;
-      axios
-        .post("/.netlify/functions/contact-add", {
-          email: this.email,
-        })
+      axios({
+        method: "post",
+        url: "https://maillist-manage.com.au/weboptin.zc",
+        data: qs.stringify({
+          CONTACT_EMAIL: this.email,
+          SIGNUP_SUBMIT_BUTTON: "Join+Now",
+          submitType: "optinCustomView",
+          emailReportId: "",
+          formType: "QuickForm",
+          zx: "11a152b97a",
+          zcvers: "2.0",
+          oldListIds: "",
+          mode: "OptinCreateView",
+          zcld: "1200abb933ac08a",
+          zctd: "1200abb933abfa1",
+          zc_trackCode: "ZCFORMVIEW",
+          zc_formIx:
+            "3zbb11adb398c087415785ed56ece36092ca7821bf28f8c199b783be7eb91f2410",
+          scriptless: "yes",
+        }),
+        headers: {
+          "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      })
         .then((response) => {
-          console.log(response);
-          if (response.data.success === true) {
+          if (response.data.includes("SUCCESS")) {
             this.$emit("done");
           } else {
             alert(
-              "Oops, something went wrong! Please email me at amer@valueheads.org. " +
-                response.data.msg
+              "Oops, something went wrong! Please email me at amer@valueheads.org. "
             );
           }
         })
--- .vuepress/components/CtaShare.vue
+++ .vuepress/components/CtaShare.vue
@@ -6,18 +6,7 @@
           @click="$emit('done')"
           :href="site.link"
           target="_blank"
-          class="
-            flex
-            justify-center
-            items-center
-            w-12
-            h-12
-            text-white
-            bg-indigo-500
-            rounded-md
-            noopener
-            hover:bg-yellow-600
-          "
+          class="flex justify-center items-center w-12 h-12 text-white bg-indigo-500 rounded-md  noopener hover:bg-yellow-600"
         >
           <component
             :is="site.icon"
@@ -30,18 +19,7 @@
       <li>
         <base-share-link
           @click="$emit('done')"
-          class="
-            flex
-            justify-center
-            items-center
-            w-12
-            h-12
-            text-white
-            bg-indigo-500
-            rounded-md
-            noopener
-            hover:bg-indigo-800
-          "
+          class="flex justify-center items-center w-12 h-12 text-white bg-indigo-500 rounded-md  noopener hover:bg-indigo-800"
           title="Check out ValueHeads.org, Follow-the-Value Entrepreneurship Course. Free!"
         >
           <share-icon size="1.5x"></share-icon>
@@ -51,15 +29,7 @@
 
     <button
       @click.prevent="$emit('done')"
-      class="
-        inline-block
-        mt-5
-        text-base
-        font-medium
-        text-indigo-700
-        hover:underline
-        hover:text-indigo-500
-      "
+      class="inline-block mt-5 text-base font-medium text-indigo-700  hover:underline hover:text-indigo-500"
     >
       I posted already! 😎
     </button>
@@ -71,28 +41,33 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import BaseShareLink from "./BaseShareLink.vue";
 import { ShareIcon } from "@zhuowenli/vue-feather-icons";
 
+const postURL = encodeURIComponent("https://valueheads.org/");
+const postTitle = encodeURIComponent(
+  "Let's open-source entrepreneurship! Check out ValueHeads.org"
+);
+
 const sites = [
   {
     id: "reddit",
-    link: "https://reddit.com/submit?url=[post-url]&title=[post-title]",
+    link: `https://reddit.com/submit?url=${postURL}&title=${postTitle}`,
     icon: FontAwesomeIcon,
     faIcon: ["fab", "reddit-alien"],
   },
   {
     id: "twitter",
-    link: "https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]",
+    link: `https://twitter.com/share?url=${postURL}&text=${postTitle}&hashtags=entrepreneurship,course,awesomeness`,
     icon: FontAwesomeIcon,
     faIcon: ["fab", "twitter"],
   },
   {
     id: "linkedin",
-    link: "https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]",
+    link: `https://www.linkedin.com/shareArticle?url=${postURL}&title=${postTitle}`,
     icon: FontAwesomeIcon,
     faIcon: ["fab", "linkedin"],
   },
   {
     id: "facebook",
-    link: "https://www.facebook.com/sharer.php?u=[post-url]",
+    link: `https://www.facebook.com/sharer.php?u=${postURL}`,
     icon: FontAwesomeIcon,
     faIcon: ["fab", "facebook"],
   },

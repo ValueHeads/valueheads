b5726b854ee050bcff64f7e0bfd8678f7ffadf82
Amer <amerk86@gmail.com>
2021-10-30 14:49:52 +1000

    Back to using MailerLite. It's free.
    ActiveCampaign is too expensive.
    
    Zoho is complex, but cheap.

--- .vuepress/components/CtaForm.vue
+++ .vuepress/components/CtaForm.vue
@@ -2,7 +2,7 @@
   <div>
     <form
       @submit.prevent="submitForm()"
-      class="box-content mx-auto mt-5  text-olive-800 sm:max-w-xl sm:w-full sm:grid sm:grid-cols-2 sm:gap-4"
+      class="box-content mx-auto  text-olive-800 sm:max-w-xl sm:w-full sm:grid sm:grid-cols-2 sm:gap-4"
     >
       <div class="min-w-0">
         <label for="hero-email" class="sr-only">Email address</label>
@@ -25,14 +25,13 @@
       </div>
     </form>
 
-    <p class="mt-4 text-sm text-olive-500">
+    <!-- <p class="mt-4 text-sm text-olive-500">
       You'll receive TWO emails. Email confirmation, then my email.
-    </p>
+    </p> -->
   </div>
 </template>
 
 <script>
-const qs = require("qs");
 const axios = require("axios");
 
 // Docs https://github.com/manishsaraan/email-validator
@@ -53,36 +52,18 @@ export default {
   methods: {
     submitForm() {
       this.isSubmitting = true;
-      axios({
-        method: "post",
-        url: "https://maillist-manage.com.au/weboptin.zc",
-        data: qs.stringify({
-          CONTACT_EMAIL: this.email,
-          SIGNUP_SUBMIT_BUTTON: "Join+Now",
-          submitType: "optinCustomView",
-          emailReportId: "",
-          formType: "QuickForm",
-          zx: "11a152b97a",
-          zcvers: "2.0",
-          oldListIds: "",
-          mode: "OptinCreateView",
-          zcld: "1200abb933ac08a",
-          zctd: "1200abb933abfa1",
-          zc_trackCode: "ZCFORMVIEW",
-          zc_formIx:
-            "3zbb11adb398c087415785ed56ece36092ca7821bf28f8c199b783be7eb91f2410",
-          scriptless: "yes",
-        }),
-        headers: {
-          "content-type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-      })
+      axios
+        .post("/.netlify/functions/contact-add", {
+          email: this.email,
+        })
         .then((response) => {
-          if (response.data.includes("SUCCESS")) {
+          console.log(response);
+          if (response.data.success === true) {
             this.$emit("done");
           } else {
             alert(
-              "Oops, something went wrong! Please email me at amer@valueheads.org. "
+              "Oops, something went wrong! Please email me at amer@valueheads.org. " +
+                response.data.msg
             );
           }
         })

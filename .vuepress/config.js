90688bb8ff2ed8bf3c64b8174c7aac9a39744bbe
Amer <amerk86@gmail.com>
2021-11-02 22:51:50 +1000

    Added favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,7 +10,44 @@ module.exports = {
   title: "ValueHeads",
   description: "ValueHeads Free Value-Entrepreneurship Course and Community",
   head: [
-    ["link", { rel: "icon", href: "/images/logo.png" }],
+    // FAVICON https://realfavicongenerator.net/
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "link",
+      { rel: "mask-icon", href: "safari-pinned-tab.svg", color: "#5bbad5" },
+    ],
+    ["link", { rel: "shortcut icon", href: "favicon.ico" }],
+    ["meta", { rel: "msapplication-TileColor", content: "#3f2680" }],
+    ["meta", { rel: "msapplication-config", content: "/browserconfig.xml" }],
+    ["meta", { rel: "theme-color", content: "#3f2680" }],
+
+    // PRECONNECT
     [
       "link",
       {
@@ -39,6 +76,8 @@ module.exports = {
         crossorigin: "",
       },
     ],
+
+    // PRELOAD
     [
       "link",
       {

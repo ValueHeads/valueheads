465f46f260d926e4a8c6e2fced059a97b0e18b7d
Amer <amerk86@gmail.com>
2021-11-05 23:06:00 +1000

    kept just pattern-zigzag-xl from patterncss

--- .vuepress/components/LandingJumboMsg.vue
+++ .vuepress/components/LandingJumboMsg.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-40 text-[#462796] bg-indigo-500 md:py-52 pattern-zigzag-xl">
+  <div class="py-40 text-[#442989] bg-indigo-500 md:py-52 pattern-zigzag-xl">
     <div class="px-4 mx-auto max-w-7xl lg:px-8">
       <h2
         class="mt-4 text-4xl font-bold tracking-tight text-center text-white  sm:mt-5 sm:text-5xl lg:mt-6"
